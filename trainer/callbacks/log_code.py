18cbd5828743f0f24ace0c2514949ca302c0496a
jason-zl190 <i.liangzisheng@gmail.com>
2019-11-28 01:18:59 +0000

    fix discriminator

--- trainer/callbacks/log_code.py
+++ trainer/callbacks/log_code.py
@@ -12,12 +12,18 @@ class LogCode(tf.keras.callbacks.Callback):
         self.log_dir = log_dir
         
     def make_tarfile(self, log_dir, code_dir):
-        def exclude_function(filename):
+        def exclude_function_old(filename):
             return True if filename.endswith('.pyc') else False
+        def exclude_function(tarinfo):
+            filename = tarinfo.name
+            return None if filename.endswith('.pyc') else tarinfo
         
         filepath = '{}.tar.gz'.format(str(time.time()), 'w:gz')
         with tarfile.open(filepath, 'w:gz') as tar:
-            tar.add(code_dir, arcname=os.path.basename(code_dir), exclude=exclude_function)
+            if sys.version_info[1] >= 7: #python3.7
+                tar.add(code_dir, arcname=os.path.basename(code_dir), filter=exclude_function)
+            else:
+                tar.add(code_dir, arcname=os.path.basename(code_dir), exclude=exclude_function) 
         with file_io.FileIO(filepath, mode='rb') as input_f:
             with file_io.FileIO(os.path.join(log_dir, os.path.basename(filepath)), mode='wb+') as of:
                 of.write(input_f.read())
