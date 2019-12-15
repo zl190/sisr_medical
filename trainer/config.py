9547b8fe9fb1052cf0a266b8f26849bff7f3def6
jason-zl190 <i.liangzisheng@gmail.com>
2019-12-15 02:45:01 -0500

    clean the repo

--- trainer/config.py
+++ trainer/config.py
@@ -1,26 +1,57 @@
+"""
+Copyright Zisheng Liang 2019 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import time
+import argparse
 
-config = type('', (), {})()
+def get_config():
+    parser = argparse.ArgumentParser()
 
-config.bs = 16
-config.in_h = 224
-config.in_w = 224
-config.in_lh = 56
-config.in_lw = 56
+    # Input parser
+    parser.add_argument('--bs',       default=8,    type=int, help='batch size')
+    parser.add_argument('--in_h',     default=224,  type=int, help='image input size height')
+    parser.add_argument('--in_w',     default=224,  type=int, help='image input size width')
+    parser.add_argument('--in_lh',     default=56,  type=int, help='low resolution image size height')
+    parser.add_argument('--in_lw',     default=56,  type=int, help='low resolution image size width')
+    parser.add_argument('--epochs',   default=100,  type=int, help='number of epochs')
+    parser.add_argument('--lr',       default=1e-4, type=float, help='learning rate')
+    parser.add_argument('--m',        default=True, type=bool, help='manual run or hp tuning')
 
-config.WGAN_GP_LAMBDA = 10.0 # gradient penalty importance
-config.COARSE_L1_ALPHA = 1.2 # importance of coarse l1
-config.L1_LOSS_ALPHA = 100 # importance of fine l1
-config.AE_LOSS_ALPHA = 1.2 # importance of full reconstruction
-config.GAN_LOSS_ALPHA = 0.01 # importance of GAN loss
-config.LOCAL = 1
-config.NUM_ITER = 5
+    
+    # dataset
+    parser.add_argument('--dataset',  default='oxford_iiit_pet', help='dataset for training')
 
-config.epochs = 100
-config.m = True
-config.lr = 1e-4
+    
+    # GAN Params
+    parser.add_argument('--L1_LOSS_ALPHA', default=1000, type=float, help='importance of fine l1')
+    parser.add_argument('--GAN_LOSS_ALPHA', default=0.001, type=float, help='importance of GAN loss')
 
-config.job_dir = 'gs://bme590/zisheng/debug/SRGAN/{}'.format(str(time.time()))
-config.model_dir = './trained_models'
+  
+    # Cloud ML Params
+    parser.add_argument('--job-dir', default='gs://duke-MML/sisr/tmp/{}'.format(str(time.time())), help='Job directory for Google Cloud ML')
+    parser.add_argument('--model_dir', default='./trained_models', help='Directory for trained models')
+    parser.add_argument('--image_dir', default=None, help='Local image directory')
+    
+    # pretrained weight
+    parser.add_argument('--g_weight', default=None, help='pretrained generator weight path')
+    parser.add_argument('--d_weight', default=None, help='pretrained discriminator weight path')
 
-config.image_dir = None
+    
+    parsed, unknown = parser.parse_known_args()
+    
+    print('Unknown args:', unknown)
+    print('Parsed args:', parsed.__dict__)
+    
+    return parsed
+
+config = get_config()
--- trainer/models/inpainting/__init__.py
+++
@@ -1,2 +0,0 @@
-from trainer.models.inpainting.models import InPainting, wgan_local_discriminator, wgan_global_discriminator
-from trainer.models.inpainting.gan import InPaintingWGAN
